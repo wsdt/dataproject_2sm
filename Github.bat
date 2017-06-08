d813bc2ed66b3e800afe79263ee37b78ea3885ae
Riedl Kevin Alfred <1610653212@fh-kufstein.ac.at>
2017-06-08 17:04:51 +0200

    github pusher.bat updated

--- Github.bat
+++ Github.bat
@@ -18,7 +18,7 @@ if "%files%" equ "." echo WARNING: ALL FILES ARE GOING TO BE OVERWRITTEN!!!
 echo.
 set message=%date%_%username%
 set /p "message=Your Commit-Message: "
-
+:doagain
 git add %files%
 git commit -m "%message%"
 git push
@@ -27,6 +27,6 @@ set "works=Y"
 echo Did it work? [Y/N]
 set /p works=[--- 
 REM Update/Merge local repository, then push your own changes. 
-if /i "%works%"=="N" git pull https://github.com/wsdt/dataproject_2sm.git&timeout 5&goto retry
+if /i "%works%"=="N" git pull https://github.com/wsdt/dataproject_2sm.git&timeout 5&goto doagain
 pause >nul
 exit
