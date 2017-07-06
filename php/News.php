73aa2b59ba3d1a083906832ede89c318254718a5
JohnnyRieder <JohnnyRieder@users.noreply.github.com>
2017-07-06 17:10:08 +0200

    Newsticker Update

--- php/News.php
+++ php/News.php
@@ -17,24 +17,53 @@ pageAuthentification(true); //Login-Page is the only exception where false shoul
 
 // Navigation einfügen und verlinken
 echo "<header>";
-    createNav();
-    echo "</header>";
-    ?>
+createNav();
+echo "</header>";
+?>
 <section>
-    <!-- Überschrift einbinden, hit the floor ist das SStylesheet für die Schriftart-->
-    <div class="text-center">
-        <br>
-        <br>
-        <div><h1>Breaking News</h1></div>
-        <!-- Text und zweite Überschrift-->
-        <h2>DATA ENGINEEER GESUCHT!</h2>
-    <p>WIR suchen dich! Bewirb dich Jetzt bei BlUE SURFACE SEEKER!
-            <br>
-            Top Aufstiegchancen und ein gutes Gehalt?
-            <br>
-            Falls du Intressiert bist, Bewirb dich unter den Kontaktdaten im <a href="impressum.php">Impressum! </a>
-    </p>
+    <div class="text-center"><h1>Breaking News</h1>
+        <form action='<?php echo $_SERVER['PHP_SELF']; ?>' method="POST">
+            Titel:     <input type="text" name="title"/><br/>
+            Text:      <textarea id="text" name="textarea" cols="35" rows="4"></textarea><br/>
+            <input type="submit" value="Absenden"/>
+        </form>
     </div>
+    <?php
+    if(!empty($_POST)) {
+        INCLUDE 'db/dbNewConnection.php';
+        $title = $_POST['title'];
+        $textarea = $_POST['textarea'];
+        if (strlen($title) > 0 && strlen($textarea) > 0) {
+            $news = "INSERT INTO News (title, newstext) VALUES ('" . $title . "','" . $textarea . "')";
+            $pushnews = mysqli_query($tunnel, $news) or DIE ("Fehler: " . mysql_error());
+            echo 'Der Eintrag war erfolgreich';
+        } else {
+            echo 'Ihre Angaben sind fehlerhaft.';
+        }
+        mysqli_close($tunnel);
+    }
+
+
+
+
+
+
+
+    ?>
+    <?php
+    INCLUDE 'db/dbNewConnection.php';
+
+    $getnews="SELECT * FROM News ORDER BY newsID DESC";
+    $ergebnis1=mysqli_query($tunnel, $getnews);
+
+    while($row1 = mysqli_fetch_array($ergebnis1))
+    {
+        echo"<h1 class='text-center'>".$row1['title']."</h1>";
+        echo"<p class='text-center'>".$row1['newstext']."</p><br><br>";
+    }
+
+    mysqli_close($tunnel);
+    ?>
 </section>
 <!-- Bild einfügen -->
 <!-- Falls der Mauszeiger auf das Bild zeigt, Wahrer Ultra anzeigen-->
